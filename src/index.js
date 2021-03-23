1cfaf7cc2eacd4f5c3959ba3bba93d9f389709fa
ulissesms <ulisses.sousa@edu.unipar.br>
2021-03-23 17:50:25 -0300

    add incomnia and created statement

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ app.use(express.json())
 
 const customers = []
 
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+  if (!customer) {
+    return res.status(400).json({error: 'Customer not found!'})
+  }
+  req.customer = customer
+  return next()
+}
 
 app.post("/account",(req, res) => {
   const {cpf, name} = req.body
@@ -27,5 +37,15 @@ app.post("/account",(req, res) => {
     statement:[]
   })
   return res.status(201).send()
+})
+
+//app.use(verifyIfExistsAccountCPF)
+
+app.get("/statement",verifyIfExistsAccountCPF,(req, res) => {
+
+  const {customer } = req
+  
+  return res.json(customer.statement)
+
 })
 app.listen(3000)
